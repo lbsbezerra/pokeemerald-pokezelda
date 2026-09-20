c8e7c3be4d854fc952936dc7243521cf1753b253
lbsbezerra <lbsbezerra@gmail.com>
2026-09-20 14:02:23 +0200

    started changed twins to Pols voice

--- src/data/object_events/object_event_pic_tables.h
+++ src/data/object_events/object_event_pic_tables.h
@@ -119,15 +119,15 @@ static const struct SpriteFrameImage sPicTable_NinjaBoy[] = {
 };
 
 static const struct SpriteFrameImage sPicTable_Twin[] = {
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 0),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 1),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 2),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 3),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 4),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 5),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 6),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 7),
-    overworld_frame(gObjectEventPic_Twin, 2, 4, 8),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 0),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 1),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 2),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 3),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 4),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 5),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 6),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 7),
+    overworld_frame(gObjectEventPic_Twin, 4, 4, 8),
 };
 
 static const struct SpriteFrameImage sPicTable_Boy1[] = {
